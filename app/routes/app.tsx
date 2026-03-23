fd3de7565ddd0fd1e1ec7329339b7704f376a3ae
Makiko Ohashi <makiron19831014@gmail.com>
2026-03-23 12:25:19 +0900

    Keep settings as the only visible subpage

--- app/routes/app.tsx
+++ app/routes/app.tsx
@@ -19,7 +19,6 @@ export default function App() {
     <AppProvider embedded apiKey={apiKey}>
       <ui-nav-menu>
         <a href="/app" rel="home">Home</a>
-        <a href="/app/sync">Sync Console</a>
         <a href="/app/settings">Settings</a>
       </ui-nav-menu>
       <Outlet />
